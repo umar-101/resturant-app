96cb4d733b6ba4ef767690f352700aeb70c492fa
UMAR <umarmushtaq101@gmail.com>
2022-06-07 12:05:17 +0500

    header added

--- src/container/Header/Header.jsx
+++ src/container/Header/Header.jsx
@@ -2,9 +2,20 @@ import React from 'react';
 
 import './Header.css';
 
+import { images } from '../../constants';
+import SubHeading from '../../components/SubHeading/SubHeading';
+
 const Header = () => (
-  <div>
-    Header
+  <div className='app__header app__wrapper section_padding' id='home'>
+    <div style={{ margin: '1rem 2rem' }} className='app__wrapper_info'>
+      <SubHeading title="Chase the new Flavour"></SubHeading>
+      <h1 className='app__header-h1'>The key to fine dining</h1>
+      <p className='p__opensans'>Sit tellus lobortis sed senectus vivamus molestie. Condimentum volutpat morbi facilisis quam scelerisque sapien. Et, penatibus aliquam amet tellus </p>
+      <button type='button' className='custom__button'>Explore Menu</button>
+    </div>
+    <div className='app__wrapper_img'>
+      <img src={images.welcome} alt="headerimg" />
+    </div>
   </div>
 );
 
